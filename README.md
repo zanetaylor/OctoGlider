d7c20c462c42fd83cec5397937186bfdd21da04c
Tom Creighton <t@tomcreighton.com>
2012-10-28 14:14:55 -0400

    Glider theme files.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 Glider
 ======
 
-A minimal, responsive text-focused theme for Wordpress.
+**A minimal, responsive, text-focused theme for Wordpress.**
+
+Glider is a one-template-file Wordpress theme that uses jQuery's .load() function to ping Wordpress for single posts, then inline-loads the post in question - this results in the ability to read all the posts without any (apparent) page loads.
+
+This theme is ideal for people (like myself) who write fairly infrequently, as *all* posts are listed in the archive section. That said, it's easy to add a category restriction to the archive query.
+
+When a post is loaded, the post-slug is added to the URL as a hash. The theme checks to see if a hash is present when URLs are loaded, if so, it loads the correct/requested post via normal permalink resolution. The description in the header block, above the post, uses Wordpress' built-in description, editable in Settings > General.
---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title><?php echo get_bloginfo('name'); ?></title>
+
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/jquery.effects.core.js"></script>
+<script src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/jquery.effects.slide.js"></script>
+
+<link href='http://fonts.googleapis.com/css?family=Alegreya:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
+
+
+
+<link href="<?php echo get_stylesheet_uri(); ?>" rel="stylesheet" type="text/css" />
+
+<script>
+	if(window.location.hash) {
+		var hash = window.location.hash;
+		var hashtourl = hash.substring(2)
+		window.location.href = "/"+hashtourl;
+	}
+</script>
+
+
+</head>
+
+<body>
+
+<a class="shift" id="goarchive">&rarr;</a>
+<a class="shift" id="gopost">&larr;</a>
+
+
+<ul id="content">
+	<li id="post">
+		
+		<div class="content">
+			
+			<?php $my_query = new WP_Query($query_string . "&showposts=1"); while ($my_query->have_posts()) : $my_query->the_post(); $do_not_duplicate = $post->ID; ?>
+				
+				<p id="hello">
+					 <?php echo get_bloginfo('description'); ?> This blog post was published on <?php the_time('F j Y') ?>. <a href="<?php the_permalink(); ?>">&sect;</a> 
+				</p>
+			
+			
+				<h1><?php the_title(); ?></h1>
+							
+				<?php the_content(); ?>
+					
+			<?php endwhile; ?>
+			
+		</div>
+		
+		<div id="footer">
+			Using the <a href="http://tomcreighton.com/glider-theme">Glider</a> theme.
+		</div>
+												
+	</li>
+	
+	<li id="archive">
+		
+		<div class="content">
+			
+			<ul>
+				<?php $my_query = new WP_Query("showposts=999999"); while ($my_query->have_posts()) : $my_query->the_post(); $do_not_duplicate = $post->ID; ?>
+					<li><h2><a rel="<? the_permalink(); ?>" id="<? the_id(); ?>" title="<?php echo( basename(get_permalink()) ); ?>"><?php the_title(); ?></a></h2> <span><?php the_time('F j Y') ?></span></li>
+				<?php endwhile; ?>
+			</ul>
+			
+		</div>
+												
+	</li>
+	
+</ul>
+
+
+
+<script>
+	$(document).ready(function() {
+		
+		function goarchive() {
+			$('#goarchive').fadeOut(300);
+			$('#post').hide('slide', {direction: 'left'}, 600, function() {
+				$("#archive").scrollTop(0);
+				$('#archive').show('slide', {direction: 'right'}, 600);
+				$('#gopost').fadeIn(300);
+			});
+		};
+		
+		function gopost() {
+			$('#gopost').fadeOut(300);
+			$('#archive').hide('slide', {direction: 'right'}, 600, function() {
+				$("#post").scrollTop(0);
+				$('#post').show('slide', {direction: 'left'}, 600);
+				$('#goarchive').fadeIn(300);
+			});
+		};
+		
+		function loadpost() {
+			
+			var perma = $(this).attr("rel");
+			var postid = $(this).attr("id");
+			var postitle = $(this).attr("title");
+			
+			$('#'+postid).parent().parent().addClass('loader');
+			
+			$("#post").load(''+perma+' #post', function() {
+				$('#gopost').fadeOut(300);
+				$('#archive').hide('slide', {direction: 'right'}, 600, function() {
+					$("#post").scrollTop(0);
+					$('#goarchive').fadeIn(300);
+					$('#post').show('slide', {direction: 'left'}, 600, function() {
+						$('#'+postid).parent().parent().removeClass('loader');
+						window.location.hash = "/"+postitle;
+						twttr.widgets.load()
+					});
+				});
+				
+			});
+		}
+		
+		$('#goarchive').live("click", goarchive);
+		
+		$('#gopost').live("click", gopost);
+		
+		$("#archive a").live("click", loadpost);
+		
+
+						
+	});
+</script>
+
+
+</body>
+</html>
